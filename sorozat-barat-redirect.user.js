8a4f8d2a3e4bee1734831e6258b126dab4c96406
peetertoth <peeter.toth@gmail.com>
2017-09-09 17:17:48 +0200

    update url attributes

--- sorozat-barat-redirect.user.js
+++ sorozat-barat-redirect.user.js
@@ -6,8 +6,8 @@
 // @author       kyle
 // @match        http://adf.ly/*/https://www.sorozat-barat.club/video/redirect/*
 // @grant        none
-// @updateURL    https://raw.githubusercontent.com/peetertoth/sorozat-barat-redirect/master/sorozat-barat-redirect.js
-// @downloadURL  https://raw.githubusercontent.com/peetertoth/sorozat-barat-redirect/master/sorozat-barat-redirect.js
+// @updateURL    https://raw.githubusercontent.com/peetertoth/sorozat-barat-redirect/master/sorozat-barat-redirect.user.js
+// @downloadURL  https://raw.githubusercontent.com/peetertoth/sorozat-barat-redirect/master/sorozat-barat-redirect.user.js
 // ==/UserScript==
 
 (function() {
