8e04c0d812c294f6fc9dd9ac5d2fe776a3a6917b
peetertoth <peeter.toth@gmail.com>
2018-04-21 11:08:15 +0200

    update match

--- sorozat-barat-redirect.user.js
+++ sorozat-barat-redirect.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         sorozat-barat redirect
 // @namespace    peetftp.ddns.net
-// @version      0.6
+// @version      0.7
 // @description  try to take over the world!
 // @author       kyle
 // @match        http://adf.ly/*/https://www.sorozat-barat.club/video/redirect/*
 // @match        http://queuecosm.bid/*/https://www.sorozat-barat.club/video/redirect/*
 // @match        http://threadsphere.bid/*/https://www.sorozat-barat.club/video/redirect/*
 // @match        http://threadsphere.bid/*
-// @match        http://restorecosm.bid/*/
+// @match        http://restorecosm.bid/*
 // @grant        none
 // @updateURL    https://raw.githubusercontent.com/peetertoth/sorozat-barat-redirect/master/sorozat-barat-redirect.user.js
 // @downloadURL  https://raw.githubusercontent.com/peetertoth/sorozat-barat-redirect/master/sorozat-barat-redirect.user.js
